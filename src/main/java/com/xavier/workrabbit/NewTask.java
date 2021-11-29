617edab7106e9c8ba33a2b2a117c3e314eda1047
Gabriel Xavier Silva <bielsaar@gmail.com>
2021-11-29 15:51:53 -0300

    Add files via upload

---
+++ src/main/java/com/xavier/workrabbit/NewTask.java
@@ -0,0 +1,27 @@
+package com.xavier.workrabbit;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+public class NewTask {
+
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main (String [] argv) throws Exception {
+        ConnectionFactory ConnectionFactory = new ConnectionFactory ();
+        ConnectionFactory.setHost ("localhost");
+        try (Connection connection = ConnectionFactory.newConnection ();
+             Channel channel = connection.createChannel ()) {
+            channel.queueDeclare (TASK_QUEUE_NAME, true, false, false, null);
+
+            String mensagem = String.join("", argv);
+
+            channel.basicPublish ("", TASK_QUEUE_NAME,
+                    MessageProperties.PERSISTENT_TEXT_PLAIN,
+                    mensagem.getBytes("UTF-8"));
+            System.out.println ("[x] Enviado '" + mensagem + "', vindo do parametro passado no java.");
+        }
+    }
+
+}
---
+++ src/test/java/com/xavier/workrabbit/WorkRabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xavier.workrabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkRabbitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
